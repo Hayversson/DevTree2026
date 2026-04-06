57f2bd96531cc13a45b26b00c25db5981c451616
Hayversson Jhair Mosquera Moreno <hayversson10one@gmail.com>
2026-04-06 16:38:58 -0500

    feat: update database configuration

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,23 @@
 import { Router } from 'express';
+import User from './models/User';
 
 const router = Router();
 
 // Routing for authentication and register:
-router.post('/auth/register', (req, res) => {
-    res.send('Desde register, lo envia res.send!...');
+router.post('/auth/register', async (req, res) => {
     console.log('Desde register!...');
     console.log(req.body);
+
+    // We can select between this two seentes for create and insert data:
+    
+    // Only one sentence:
+    //await User.create(req.body);
+
+    // or with two sentences:
+    const user = new User(req.body);
+    await user.save();
+
+    res.send({ msg: 'User registered successfully!' });
 });
 
 export default router;
